252e7623a76e7d51d3fdfad07ada9b3a45239561
Hồ Ngọc Triển <hongoctrien@2mit.org>
2017-12-08 09:30:48 +0700

    Cấu trúc module

---
+++ modules/tdfoss/version.php
@@ -0,0 +1,26 @@
+<?php
+
+/**
+ * @Project NUKEVIET 4.x
+ * @Author mynukeviet (contact@mynukeviet.com)
+ * @Copyright (C) 2017 mynukeviet. All rights reserved
+ * @Createdate Fri, 08 Dec 2017 02:19:21 GMT
+ */
+
+if (!defined('NV_MAINFILE')) die('Stop!!!');
+
+$module_version = array(
+    'name' => 'Tdfoss',
+    'modfuncs' => '',
+    'change_alias' => '',
+    'submenu' => '',
+    'is_sysmod' => 0,
+    'virtual' => 1,
+    'version' => '1.0.00',
+    'date' => 'Fri, 8 Dec 2017 02:19:21 GMT',
+    'author' => 'mynukeviet (contact@mynukeviet.com)',
+    'uploads_dir' => array(
+        $module_name
+    ),
+    'note' => ''
+);
