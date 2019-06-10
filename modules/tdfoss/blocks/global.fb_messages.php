d25a06dd56e92cf0b0267bc62a254caf69e724f9
Hồ Ngọc Triển <hongoctrien@tdfoss.vn>
2019-06-10 10:47:41 +0700

    Cập nhật block fb_messenger

--- modules/tdfoss/blocks/global.fb_messages.php
+++ modules/tdfoss/blocks/global.fb_messages.php
@@ -7,7 +7,6 @@
  * @License GNU/GPL version 2 or any later version
  * @Createdate Jun 26, 2016 11:34:27 AM
  */
-
 if (!defined('NV_MAINFILE')) die('Stop!!!');
 
 if (!nv_function_exists('nv_fb_chat')) {
@@ -16,15 +15,18 @@ if (!nv_function_exists('nv_fb_chat')) {
     {
         global $lang_global, $selectthemes;
 
-        $html = '<tr>';
-        $html .= '<td>App Id</td>';
-        $html .= '<td><input type="text" class="form-control" name="config_appId" value="' . $data_block['appId'] . '"></td>';
-        $html .= '</tr>';
-        $html .= '<tr>';
-        $html .= '<td>Page id</td>';
-        $html .= '<td><input type="text" class="form-control" name="config_page_id" value="' . $data_block['page_id'] . '"></td>';
-        $html .= '</tr>';
-
+        $html = '<div class="form-group">';
+        $html .= '<label class="control-label col-sm-6">Pageid</label>';
+        $html .= '<div class="col-sm-18"><input type="text" class="form-control" name="config_page_id" value="' . $data_block['page_id'] . '"></div>';
+        $html .= '</div>';
+        $html .= '<div class="form-group">';
+        $html .= '<label class="control-label col-sm-6">' . $lang_block['logged_in_greeting'] . ':</label>';
+        $html .= '<div class="col-sm-18"><input type="text" class="form-control" name="config_logged_in_greeting" value="' . $data_block['logged_in_greeting'] . '"></div>';
+        $html .= '</div>';
+        $html .= '<div class="form-group">';
+        $html .= '<label class="control-label col-sm-6">' . $lang_block['logged_out_greeting'] . ':</label>';
+        $html .= '<div class="col-sm-18"><input type="text" class="form-control" name="config_logged_out_greeting" value="' . $data_block['logged_out_greeting'] . '"></div>';
+        $html .= '</div>';
         return $html;
     }
 
@@ -34,8 +36,9 @@ if (!nv_function_exists('nv_fb_chat')) {
 
         $return = array();
         $return['error'] = array();
-        $return['config']['appId'] = $nv_Request->get_title('config_appId', 'post');
         $return['config']['page_id'] = $nv_Request->get_title('config_page_id', 'post');
+        $return['config']['logged_in_greeting'] = $nv_Request->get_title('config_logged_in_greeting', 'post');
+        $return['config']['logged_out_greeting'] = $nv_Request->get_title('config_logged_out_greeting', 'post');
 
         return $return;
     }
@@ -61,8 +64,7 @@ if (!nv_function_exists('nv_fb_chat')) {
         $xtpl = new XTemplate('global.fb_messages.tpl', NV_ROOTDIR . '/themes/' . $block_theme . '/modules/tdfoss');
         $xtpl->assign('LANG', $lang_global);
         $xtpl->assign('NV_BASE_SITEURL', NV_BASE_SITEURL);
-        $xtpl->assign('APPID', $block_config['appId']);
-        $xtpl->assign('PAGEID', $block_config['page_id']);
+        $xtpl->assign('CONFIG', $block_config);
         $xtpl->parse('main');
         return $xtpl->text('main');
     }
--- themes/default/modules/tdfoss/global.fb_messages.tpl
+++ themes/default/modules/tdfoss/global.fb_messages.tpl
@@ -1,23 +1,28 @@
 <!-- BEGIN: main -->
+<!-- Load Facebook SDK for JavaScript -->
+<div id="fb-root"></div>
 <script>
-    window.fbAsyncInit = function() {
-        FB.init({
-            appId : '{APPID}',
-            autoLogAppEvents : true,
-            xfbml : true,
-            version : 'v2.11'
-        });
-    };
-    (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) {
-            return;
-        }
-        js = d.createElement(s);
-        js.id = id;
-        js.src = "https://connect.facebook.net/vi_VN/sdk.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
-</script>
-<div class="fb-customerchat" page_id="{PAGEID}"></div>
+  window.fbAsyncInit = function() {
+    FB.init({
+      xfbml            : true,
+      version          : 'v3.3'
+    });
+  };
+
+  (function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+
+<!-- Your customer chat code -->
+<div class="fb-customerchat"
+  attribution=setup_tool
+  page_id="{CONFIG.page_id}"
+  logged_in_greeting="{CONFIG.logged_in_greeting}"
+  logged_out_greeting="{CONFIG.logged_out_greeting}">
+</div>
 <!-- END: main -->
+
